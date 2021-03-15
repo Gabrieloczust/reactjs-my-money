9a73d28d6eeadc488f42d05fc6550ee81b101d25
Gabriel Oczust <gabriel.oczust@gmail.com>
2021-03-15 16:52:42 -0300

    # Created Components - Header - Dashboard - Summary

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -1,54 +1,55 @@
-import { createGlobalStyle } from 'styled-components'
+import { createGlobalStyle } from "styled-components";
 
 export const GlobalStyle = createGlobalStyle`
     :root {
-        --red: #e52e4d;
-        --blue: #5429cc;
-        --blue-light: #6933ff;
-        
-        --text-title: #363f5f;
-        --text-body: #969cb3;
+      --red: #e52e4d;
+      --green: #33cc95;
+      --blue: #5429cc;
+      --blue-light: #6933ff;
 
-        --background: #f0f2f5; 
-        --shape: #ffffff;
+      --text-title: #363f5f;
+      --text-body: #969cb3;
+
+      --background: #f0f2f5;
+      --shape: #ffffff;
     }
 
     * {
-        margin: 0;
-        padding: 0;
-        box-sizing: border-box;
+      margin: 0;
+      padding: 0;
+      box-sizing: border-box;
     }
 
     html {
-        @media(max-width: 1080px) {
-            font-size: 93.75%;
-        }
+      @media(max-width: 1080px) {
+        font-size: 93.75%;
+      }
 
-        @media(max-width: 720px) {
-            font-size: 87.5%;
-        }
+      @media(max-width: 720px) {
+          font-size: 87.5%;
+      }
     }
 
     body {
-        background: var(--background);
-        -webkit-font-smoothing: antialiased;
+      background: var(--background);
+      -webkit-font-smoothing: antialiased;
     }
 
     body, input, textarea, button {
-        font-family: 'Poppins', sans-serif;
-        font-weight: 400;
+      font-family: 'Poppins', sans-serif;
+      font-weight: 400;
     }
 
     h1, h2, h3, h4, h5, h6, strong, b {
-        font-weight: 600;
+      font-weight: 600;
     }
 
     button {
-        cursor: pointer;
+      cursor: pointer;
     }
 
     [disabled] {
-        cursor: not-allowed;
-        opacity: 0.6;
+      cursor: not-allowed;
+      opacity: 0.6;
     }
-`
+`;
